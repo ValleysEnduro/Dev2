daf3f18b6051c1a00fa7baf573340e9c391de78c
ValleysEnduro <info@western-bike-events.wales>
2024-03-29 17:09:57 +0000

    brought back the hero image

--- core/views.py
+++ core/views.py
@@ -3,10 +3,13 @@ from .models import HomePage
 from blog.models import Post  # Adjust based on your actual model import
 
 def homepage_view(request):
-    homepage_content = HomePage.objects.first()  # Fetch the first (and likely only) instance
-    # Fetch some blog posts, e.g., the latest 5 posts
+    homepage_content = HomePage.objects.first()  # Assuming there's at least one HomePage instance
     posts = Post.objects.all().order_by('-created_on')[:5]
-    return render(request, 'homepage.html', {'posts': posts})
+    context = {
+        'homepage_content': homepage_content,  # Make sure this is correct
+        'posts': posts
+    }
+    return render(request, 'homepage.html', context)
 
 
 
