e5e3d26ec2d3ac3e1ab048a4207b25cce9767f9c
ValleysEnduro <info@western-bike-events.wales>
2024-03-29 20:02:30 +0000

    SEO friendly slug path

--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from .views import post_list, post_detail
 from . import views
 
+# blog/urls.py
 urlpatterns = [
-    path('', post_list, name='post_list'),
-    # Updating the path to use slugs
-    path('post/<slug:slug>/', views.post_detail, name='post_detail'),
+    # Other patterns...
+    path('<slug:slug>/', views.post_detail, name='post_detail'),
 ]
+
