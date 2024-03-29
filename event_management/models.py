fe03395af6b877a684bd1b626ecf054b0f1af856
ValleysEnduro <info@western-bike-events.wales>
2024-03-29 13:53:49 +0000

    hero image field and web p compression added to event model

--- event_management/models.py
+++ event_management/models.py
@@ -1,12 +1,20 @@
 from django.db import models
 from treebeard.mp_tree import MP_Node
 from age_categories.models import AgeCategory  # Import the AgeCategory model
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFill
 
 class Venue(MP_Node):
     name = models.CharField(max_length=100)
     location = models.CharField(max_length=255)
     latitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True)
     longitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True)
+    hero_image = ProcessedImageField(upload_to='venues/',
+                                     processors=[ResizeToFill(1024, 768)],
+                                     format='WEBP',
+                                     options={'quality': 90},
+                                     blank=True,
+                                     null=True)
 
     node_order_by = ['name']
 
