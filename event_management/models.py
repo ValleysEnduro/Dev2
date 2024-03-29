272e4bd72a723ebcd59ba762d9c0017a9db93939
ValleysEnduro <info@western-bike-events.wales>
2024-03-29 13:33:59 +0000

    updated venue model

--- event_management/models.py
+++ event_management/models.py
@@ -5,6 +5,8 @@ from age_categories.models import AgeCategory  # Import the AgeCategory model
 class Venue(MP_Node):
     name = models.CharField(max_length=100)
     location = models.CharField(max_length=255)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True)
 
     node_order_by = ['name']
 
