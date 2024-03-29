53fbd9367a846582c8820efb1f89bd2e51bbcf60
ValleysEnduro <info@western-bike-events.wales>
2024-03-29 15:59:50 +0000

    fixed browser tab showing page title

--- core/models.py
+++ core/models.py
@@ -3,13 +3,16 @@ from imagekit.models import ProcessedImageField
 from imagekit.processors import ResizeToFill
 
 class HomePage(models.Model):
+    page_title = models.CharField(max_length=255, blank=True, verbose_name="Page Title")
     hero_title = models.CharField(max_length=255, blank=True)
     hero_button_text = models.CharField(max_length=255, blank=True)
     hero_button_link = models.URLField(blank=True)
-    hero_image = ProcessedImageField(upload_to='hero_images/',
-                                     processors=[ResizeToFill(1920, 1080)],
-                                     format='WEBP',
-                                     options={'quality': 80})
+    hero_image = ProcessedImageField(
+        upload_to='hero_images/',
+        processors=[ResizeToFill(1920, 1080)],
+        format='WEBP',
+        options={'quality': 80}
+    )
 
     def __str__(self):
-        return "Homepage Content"
+        return self.page_title or "Homepage"
--- core/views.py
+++ core/views.py
@@ -1,11 +1,7 @@
-# core/views.py
 from django.shortcuts import render
 from .models import HomePage
 
 def homepage_view(request):
-    homepage_content = HomePage.objects.first()  # Fetch the first (and likely only) instance
-    # Just use 'homepage.html' without 'core/templates/'
+    homepage_content = HomePage.objects.first()
     return render(request, 'homepage.html', {'homepage_content': homepage_content})
-
-
 
