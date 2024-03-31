fbacb85108b38faff92c58673155a56ae9599797
ValleysEnduro <info@western-bike-events.wales>
2024-03-31 13:53:11 +0100

    added high level reporting to venue

--- event_management/admin.py
+++ event_management/admin.py
@@ -3,6 +3,12 @@ from django.urls import reverse
 from django.utils.html import format_html
 from .models import Venue, Event, Race, Entry
 from treebeard.admin import TreeAdmin
+from django.contrib import admin
+from django.db.models import Count, Prefetch, Q
+from django.urls import reverse
+from django.utils.html import format_html
+from .models import Venue, Event, Race, Entry
+from treebeard.admin import TreeAdmin
 from django import forms
 
 
@@ -23,14 +29,33 @@ class EntryInline(admin.TabularInline):
 
 # Admin class for Venue
 class VenueAdmin(admin.ModelAdmin):
-    list_display = ('name', 'view_events_link',)
-    inlines = [EventInline]
+    list_display = ('name', 'location', 'total_events', 'total_races', 'total_entries', 'view_events_link')
+    
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        # Prefetch events and races to minimize database hits, while annotating events with the count of races
+        events_prefetch = Prefetch('events', queryset=Event.objects.annotate(total_races_count=Count('races')))
+        queryset = queryset.prefetch_related(events_prefetch)
+        return queryset
+
+    def total_events(self, obj):
+        return obj.events.count()
+    total_events.short_description = 'Total Events'
+    
+    def total_races(self, obj):
+        # Since we've already prefetched and annotated events with their races count, sum those up
+        return sum(event.total_races_count for event in obj.events.all())
+    total_races.short_description = 'Total Races'
+
+    def total_entries(self, obj):
+        # Aggregate total entries across all races in all events for this venue
+        return Entry.objects.filter(race__event__venue=obj).count()
+    total_entries.short_description = 'Total Entries'
 
     def view_events_link(self, obj):
         url = reverse('admin:event_management_event_changelist') + f'?venue__id__exact={obj.pk}'
         return format_html('<a href="{}">View Events</a>', url)
     view_events_link.short_description = "Events"
-
 # Admin class for Event
 class EventAdmin(TreeAdmin, admin.ModelAdmin):  # Note: TreeAdmin is used if you want to keep tree functionalities
     list_display = ('name', 'get_depth', 'get_numchild', 'get_numraces', 'get_numentries')
