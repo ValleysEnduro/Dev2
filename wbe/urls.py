f66b1f0a724c6799447fcbb3b957f329c5b21394
ValleysEnduro <info@western-bike-events.wales>
2024-05-01 10:50:18 +0100

    Update urls.py

--- wbe/urls.py
+++ wbe/urls.py
@@ -33,13 +33,3 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-# urls.py
-from django.urls import path
-
-def trigger_error(request):
-    division_by_zero = 1 / 0
-
-urlpatterns = [
-    path('sentry-debug/', trigger_error),
-    # ...
-]
