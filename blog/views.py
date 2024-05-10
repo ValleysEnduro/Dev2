e3fc64d11830487bc5a20057045f57311805bdd4
ValleysEnduro <info@western-bike-events.wales>
2024-05-10 20:49:24 +0100

    fixed security issue in view.py

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.decorators.http import require_http_methods
 from .models import Post
+from django.core.paginator import Paginator
 
+@require_http_methods(["GET"])
 def post_list(request):
     posts = Post.objects.all().order_by('-created_on')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    
+    # Adding pagination
+    paginator = Paginator(posts, 10)  # Show 10 posts per page
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    
+    return render(request, 'blog/post_list.html', {'page_obj': page_obj})
 
+@require_http_methods(["GET"])
 def post_detail(request, slug):
-    # Notice we're now using 'slug' instead of 'post_id'
     post = get_object_or_404(Post, slug=slug)
     return render(request, 'blog/post_detail.html', {'post': post})
