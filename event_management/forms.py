bb3c62a92b2e89882dc335775106777a4b99034d
ValleysEnduro <info@western-bike-events.wales>
2024-05-11 19:24:36 +0100

    removed circular referernce

--- event_management/forms.py
+++ event_management/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Entry, TermsandConditions
+from django.apps import apps
 
 class EntryForm(forms.ModelForm):
     class Meta:
@@ -28,6 +29,20 @@ class EntryForm(forms.ModelForm):
             'club_team_name': forms.TextInput(),
             # Add any other specific widgets if needed
         }
+        
+class CustomUserCreationForm(forms.ModelForm):
+    class Meta:
+        model = apps.get_model('event_management', 'Entry')  # Dynamically reference Entry model
+        fields = '__all__'
+
+# Functions to dynamically get models if needed
+def get_entry_model():
+    from .models import Entry
+    return Entry
+
+def get_terms_and_conditions_model():
+    from .models import TermsandConditions
+    return TermsandConditions
 #When you want to link to this form within your templates or redirect to it in your views, you can refer to it using the namespaced URL name, like so
 
 #<a href="{% url 'event_management:enter_race' %}">Enter a Race</a>
