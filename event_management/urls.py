ceefaff81b9ed1d5057aa88bc9d20a54e6d98519
ValleysEnduro <info@western-bike-events.wales>
2024-05-12 22:23:13 +0100

    Update urls.py

--- event_management/urls.py
+++ event_management/urls.py
@@ -7,4 +7,5 @@ app_name = 'event_management'  # This is useful for namespacing your URLs
 urlpatterns = [
     path('submit/<int:race_id>/', views.submit_entry_form, name='submit_entry_form'),
     path('entry/submit/<int:race_id>/', views.submit_entry_form, name='submit_entry_form'),
+    path('race/<int:race_id>/entry/', views.submit_entry_form, name='entry_form')
 ]
