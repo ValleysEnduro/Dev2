98c8c33d4167c355f137b3630b1b0356f184d337
ValleysEnduro <info@western-bike-events.wales>
2024-05-29 20:29:09 +0100

    Update createsuperuser_if_none.py

--- core/management/commands/createsuperuser_if_none.py
+++ core/management/commands/createsuperuser_if_none.py
@@ -2,6 +2,9 @@ from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
 from django.conf import settings
 import logging
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+from django.conf import settings
 
 logger = logging.getLogger(__name__)
 
@@ -27,3 +30,31 @@ class Command(BaseCommand):
         else:
             self.stdout.write(self.style.SUCCESS('Superuser already exists.'))
             logger.info('Superuser already exists.')
+
+class Command(BaseCommand):
+    help = 'List all superusers'
+
+    def handle(self, *args, **kwargs):
+        User = get_user_model()
+        superusers = User.objects.filter(is_superuser=True)
+        if superusers.exists():
+            for user in superusers:
+                self.stdout.write(self.style.SUCCESS(f'Superuser: {user.username} ({user.email})'))
+        else:
+            self.stdout.write(self.style.WARNING('No superusers found'))
+
+class Command(BaseCommand):
+    help = 'Reset the superuser password'
+
+    def handle(self, *args, **kwargs):
+        User = get_user_model()
+        username = settings.SUPERUSER_USERNAME
+        password = settings.SUPERUSER_PASSWORD
+        
+        try:
+            user = User.objects.get(username=username, is_superuser=True)
+            user.set_password(password)
+            user.save()
+            self.stdout.write(self.style.SUCCESS(f'Successfully reset password for superuser: {username}'))
+        except User.DoesNotExist:
+            self.stdout.write(self.style.ERROR(f'Superuser with username {username} does not exist'))           
