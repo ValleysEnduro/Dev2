4a3103b24150b15be9450e241e5080aa3b6a06e0
ValleysEnduro <info@western-bike-events.wales>
2024-05-29 17:02:37 +0100

    deploy it

--- wbe/settings.py
+++ wbe/settings.py
@@ -24,7 +24,10 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".vercel.app", "localhost",]
+ALLOWED_HOSTS = [".vercel.app", 
+                 "localhost",
+                 "127.0.0.1",
+                 ]
 
 
 # Application definition
