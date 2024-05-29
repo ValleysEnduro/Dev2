e79f9bfe7cdee9bb536e67535ff2e3fa7ed94d50
ValleysEnduro <info@western-bike-events.wales>
2024-05-29 12:30:06 +0100

    Update wsgi.py

--- wbe/wsgi.py
+++ wbe/wsgi.py
@@ -1,11 +1,3 @@
-"""
-WSGI config for wbe project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
-"""
 
 import os
 
@@ -14,3 +6,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wbe.settings')
 
 application = get_wsgi_application()
+
+app = application
