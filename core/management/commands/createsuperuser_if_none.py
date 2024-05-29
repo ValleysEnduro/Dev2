c7f6348f9ff1bf14bd488043871f7f7de9a1344a
ValleysEnduro <info@western-bike-events.wales>
2024-05-29 19:19:21 +0100

    Update createsuperuser_if_none.py

---
+++ core/management/commands/createsuperuser_if_none.py
@@ -0,0 +1,17 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+from django.conf import settings
+
+class Command(BaseCommand):
+    help = 'Create a superuser if none exists'
+
+    def handle(self, *args, **kwargs):
+        User = get_user_model()
+        if not User.objects.filter(is_superuser=True).exists():
+            username = settings.SUPERUSER_USERNAME
+            email = settings.SUPERUSER_EMAIL
+            password = settings.SUPERUSER_PASSWORD
+            User.objects.create_superuser(username=username, email=email, password=password)
+            self.stdout.write(self.style.SUCCESS('Superuser created.'))
+        else:
+            self.stdout.write(self.style.SUCCESS('Superuser already exists.'))
