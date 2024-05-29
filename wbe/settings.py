744836e8f5ad715b39d5770ea5f3b41454b934d8
ValleysEnduro <info@western-bike-events.wales>
2024-05-29 14:16:23 +0100

    deploy it

--- wbe/settings.py
+++ wbe/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'wbe.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': [BASE_DIR / 'db.sqlite3']
     }
 }
 
