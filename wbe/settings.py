8abcfc982a42512c90b727ad4fe4875fae2ab762
ValleysEnduro <info@western-bike-events.wales>
2024-05-29 17:45:12 +0100

    Update settings.py

--- wbe/settings.py
+++ wbe/settings.py
@@ -142,6 +142,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
