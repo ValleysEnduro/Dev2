299af311b7456822b75ef0962d384fc3be66b465
ValleysEnduro <info@western-bike-events.wales>
2024-05-29 14:40:30 +0100

    Update wsgi.py

--- wbe/wsgi.py
+++ wbe/wsgi.py
@@ -6,8 +6,11 @@ from whitenoise import WhiteNoise
 # Define BASE_DIR to match the definition in settings.py
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wbe.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wbe.settings')  # Replace 'myproject.settings' with your actual settings module
 
 application = get_wsgi_application()
 application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'staticfiles'))
 application.add_files(os.path.join(BASE_DIR, 'static'), prefix='static/')
+
+# Expose WSGI application as `app`
+app = application
