cff9506024ff3a13b04245b3d4ab0313c458e5a4
ValleysEnduro <info@western-bike-events.wales>
2024-05-30 10:08:08 +0100

    push

--- wbe/wsgi.py
+++ wbe/wsgi.py
@@ -1,6 +1,10 @@
 import os
+import sys
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wbe.settings')  # Ensure this matches your settings module
+sys.stdout.write("WSGI application module loaded.\n")
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wbe.settings')
 
 application = get_wsgi_application()
+sys.stdout.write("WSGI application set.\n")
